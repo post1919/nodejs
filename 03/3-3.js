d4b34b6787b8ac53f4cd389fc308794acda161f3
post1919 <post1919@naver.com>
2019-01-14 22:18:32 +0900

    Initial commit

---
+++ 03/3-3.js
@@ -0,0 +1,8 @@
+console.time('alpha');
+
+var output = 1;
+for( var i=1; i<=10; i++ ){
+	output += i;
+}
+
+console.log('Result : %d', output);
---
+++ 03/3-6.js
@@ -0,0 +1,7 @@
+console.log('- process.env : ', process.env);
+console.log('- process.version : ', process.version);
+console.log('- process.version : ', process.versions);
+console.log('- process.arch : ', process.arch);
+console.log('- process.platform : ', process.platform);
+console.log('- process.memoryUsage() : ', process.memoryUsage());
+console.log('- process.uptime() : ', process.uptime());
