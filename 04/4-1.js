11cae14c41d002d1dede8d20f87413613715f5a7
post1919 <post1919@naver.com>
2019-01-14 23:32:19 +0900

    os모듈 함수들 추가

--- 04/4-1.js
+++ 04/4-1.js
@@ -1,2 +1,13 @@
 //모듈을 추출합니다
-var os = require('OS');
+var os = require('os');
+
+console.log('hostname() : ', os.hostname());
+console.log('os.type() : ' + os.type());
+console.log('os.platform() : ' + os.platform());
+console.log('os.release() : ', os.release());
+console.log('os.uptime() : ', os.uptime());
+console.log('os.loadavg() : ', os.loadavg());
+console.log('os.totalmem() : ', os.totalmem());
+console.log('os.freemem() : ', os.freemem());
+console.log('os.cpus() : ', os.cpus());
+console.log('os.getNetworkInterfaces() : ', os.getNetworkInterfaces());
